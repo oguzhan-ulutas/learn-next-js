6378ae18e7e2178a603b49e865e0953709db246c
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-05-29 16:18:15 +0300

    Add profiler

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ButtonComponent from './components/ButtonComponent'
+import { Profiler, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+
+const onRender = (
+  id, // Profiler ID
+  phase, // "mount" (for the initial render) or "update" (for re-renders)
+  actualDuration, // Time spent rendering the committed update
+  baseDuration, // Estimated time to render the entire subtree without memoization
+  startTime, // When React began rendering this update
+  commitTime // When React committed this update
+) => {
+  console.log(
+    `[Profiler] id: ${id}, ${phase} phase: ${actualDuration}ms, baseDuration: ${baseDuration}ms, startTime: ${startTime}, commitTime: ${commitTime}.`
+  );
+};
 
 function App() {
-  
-
   return (
     <>
-      <ButtonComponent/>
+      <Profiler id="buttonComponent" onRender={onRender}>
+        <ButtonComponent />
+      </Profiler>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Profiler } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const onRender = (
+  id, // Profiler ID
+  phase, // "mount" (for the initial render) or "update" (for re-renders)
+  actualDuration, // Time spent rendering the committed update
+  baseDuration, // Estimated time to render the entire subtree without memoization
+  startTime, // When React began rendering this update
+  commitTime // When React committed this update
+) => {
+  console.log(
+    `[Profiler] id: ${id}, ${phase} phase: ${actualDuration}ms, baseDuration: ${baseDuration}ms, startTime: ${startTime}, commitTime: ${commitTime}.`
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Profiler id="App" onRender={onRender}>
+      <App />
+    </Profiler>
+  </React.StrictMode>
+);

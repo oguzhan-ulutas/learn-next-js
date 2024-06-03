d05f90f48cbef322a4daeb9d6b3d75837ce0aae1
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-06-03 19:27:51 +0300

    Add memo

--- my-first-react-app/src/components/hooks/ChildMemo.jsx
+++ my-first-react-app/src/components/hooks/ChildMemo.jsx
@@ -1,9 +1,10 @@
 import { memo } from "react";
 
-const ChildMemo = ({ counter }) => {
+const ChildMemo = ({ counter, updateCounter }) => {
   console.log("Child is rendering!!!");
   return (
     <div style={{ margin: "20px", border: "1px solid blue", padding: "20px" }}>
+      <button onClick={updateCounter}>increment</button>
       ChildMemo {counter}
     </div>
   );
--- my-first-react-app/src/components/hooks/Memo.jsx
+++ my-first-react-app/src/components/hooks/Memo.jsx
@@ -1,10 +1,13 @@
-import { useCallback, useState, memo } from "react";
+import { useCallback, useState, memo, useMemo } from "react";
 import { Link } from "react-router-dom";
 import List from "./List";
 import { ChildMemoMemorized } from "./ChildMemo";
 
 function Memo() {
   const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("");
+
+  const updateCounter = useCallback(() => setCounter(counter + 1), [counter]);
 
   return (
     <div style={{ border: "1px solid red" }}>
@@ -17,7 +20,14 @@ function Memo() {
       >
         Increment Counter
       </button>
-      <ChildMemoMemorized counter={counter} />
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <ChildMemoMemorized counter={counter} updateCounter={updateCounter} />
 
       <Link to="/">
         <button>Return Home</button>
